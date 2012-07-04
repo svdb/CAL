315b187e026ce8e612f7f1e8da332f0a46ead922
svdb <svdb@coketools.com>
2012-07-04 14:55:28 -0700

    init

---
+++ blog/locales/ja/date.lang.php
@@ -0,0 +1,58 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2011 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------#
+#
+#
+#        DOT NOT MODIFY THIS FILE !
+
+
+
+
+$GLOBALS['__l10n']['%Y-%m-%d %H:%M'] = '%Y-%m-%d %H:%M';
+$GLOBALS['__l10n']['_Jan'] = '1月';
+$GLOBALS['__l10n']['_Feb'] = '2月';
+$GLOBALS['__l10n']['_Mar'] = '3月';
+$GLOBALS['__l10n']['_Apr'] = '4月';
+$GLOBALS['__l10n']['_May'] = '5月';
+$GLOBALS['__l10n']['_Jun'] = '6月';
+$GLOBALS['__l10n']['_Jul'] = '7月';
+$GLOBALS['__l10n']['_Aug'] = '8月';
+$GLOBALS['__l10n']['_Sep'] = '9月';
+$GLOBALS['__l10n']['_Oct'] = '10月';
+$GLOBALS['__l10n']['_Nov'] = '11月';
+$GLOBALS['__l10n']['_Dec'] = '12月';
+$GLOBALS['__l10n']['January'] = '1月';
+$GLOBALS['__l10n']['February'] = '2月';
+$GLOBALS['__l10n']['March'] = '3月';
+$GLOBALS['__l10n']['April'] = '4月';
+$GLOBALS['__l10n']['May'] = '5月';
+$GLOBALS['__l10n']['June'] = '6月';
+$GLOBALS['__l10n']['July'] = '7月';
+$GLOBALS['__l10n']['August'] = '8月';
+$GLOBALS['__l10n']['September'] = '9月';
+$GLOBALS['__l10n']['October'] = '10月';
+$GLOBALS['__l10n']['November'] = '11月';
+$GLOBALS['__l10n']['December'] = '12月';
+$GLOBALS['__l10n']['_Mon'] = '月';
+$GLOBALS['__l10n']['_Tue'] = '火';
+$GLOBALS['__l10n']['_Wed'] = '水';
+$GLOBALS['__l10n']['_Thu'] = '木';
+$GLOBALS['__l10n']['_Fri'] = '金';
+$GLOBALS['__l10n']['_Sat'] = '土';
+$GLOBALS['__l10n']['_Sun'] = '日';
+$GLOBALS['__l10n']['Monday'] = '月曜日';
+$GLOBALS['__l10n']['Tuesday'] = '火曜日';
+$GLOBALS['__l10n']['Wednesday'] = '水曜日';
+$GLOBALS['__l10n']['Thursday'] = '木曜日';
+$GLOBALS['__l10n']['Friday'] = '金曜日';
+$GLOBALS['__l10n']['Saturday'] = '土曜日';
+$GLOBALS['__l10n']['Sunday'] = '日曜日';
+?>
---
+++ blog/locales/ja/public.lang.php
@@ -0,0 +1,93 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+#
+# This file is part of Dotclear 2.
+#
+# Copyright (c) 2003-2011 Olivier Meunier & Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------#
+#
+#
+#        DOT NOT MODIFY THIS FILE !
+
+
+
+
+$GLOBALS['__l10n']['To content'] = 'コンテンツへ';
+$GLOBALS['__l10n']['To menu'] = 'メニューへ';
+$GLOBALS['__l10n']['To search'] = '検索へ';
+$GLOBALS['__l10n']['By'] = '投稿者：';
+$GLOBALS['__l10n']['by'] = '投稿者：';
+$GLOBALS['__l10n']['on'] = '投稿日：';
+$GLOBALS['__l10n']['On'] = '投稿日：';
+$GLOBALS['__l10n']['Continue reading'] = '続きを読む -';
+$GLOBALS['__l10n']['no comment'] = 'コメント(0)';
+$GLOBALS['__l10n']['one comment'] = 'コメント(1)';
+$GLOBALS['__l10n']['%d comments'] = 'コメント(%d)';
+$GLOBALS['__l10n']['no trackback'] = 'トラックバック(0)';
+$GLOBALS['__l10n']['one trackback'] = 'トラックバック(1)';
+$GLOBALS['__l10n']['%d trackbacks'] = 'トラックバック(%d)';
+$GLOBALS['__l10n']['no attachment'] = '添付(0)';
+$GLOBALS['__l10n']['one attachment'] = '添付(1)';
+$GLOBALS['__l10n']['%d attachments'] = '添付(%d)';
+$GLOBALS['__l10n']['previous entries'] = '前のエントリ';
+$GLOBALS['__l10n']['page'] = 'ページ';
+$GLOBALS['__l10n']['of'] = '/';
+$GLOBALS['__l10n']['next entries'] = '次のエントリ';
+$GLOBALS['__l10n']['Search'] = '検索';
+$GLOBALS['__l10n']['Your search for <em>%1$s</em> returned no result.'] = '<em>%1$s</em> の検索結果はありません。';
+$GLOBALS['__l10n']['Your search for <em>%1$s</em> returned <strong>%2$s</strong> result.'] = '<em>%1$s</em> の検索結果は <strong>%2$s</strong> です。';
+$GLOBALS['__l10n']['Your search for <em>%1$s</em> returned <strong>%2$s</strong> results.'] = '<em>%1$s</em> の検索結果は <strong>%2$s</strong> です。';
+$GLOBALS['__l10n']['Home'] = 'ホーム';
+$GLOBALS['__l10n']['All keywords'] = 'すべてのキーワード';
+$GLOBALS['__l10n']['Best of me'] = '私のイチ押し';
+$GLOBALS['__l10n']['Languages'] = '言語';
+$GLOBALS['__l10n']['Categories'] = 'カテゴリ';
+$GLOBALS['__l10n']['Subcategories'] = 'サブカテゴリ';
+$GLOBALS['__l10n']['Archives'] = 'アーカイブ';
+$GLOBALS['__l10n']['Links'] = 'リンク';
+$GLOBALS['__l10n']['Subscribe'] = '定期受信';
+$GLOBALS['__l10n']['Entries feed'] = 'エントリのフィード';
+$GLOBALS['__l10n']['Comments feed'] = 'コメントのフィード';
+$GLOBALS['__l10n']['This blog\'s comments Atom feed'] = 'このブログのコメントのAtomフィード';
+$GLOBALS['__l10n']['This category\'s entries Atom feed'] = 'このカテゴリのエントリのAtomフィード';
+$GLOBALS['__l10n']['This category\'s comments Atom feed'] = 'このカテゴリのコメントのAtomフィード';
+$GLOBALS['__l10n']['This post\'s comments feed'] = 'この投稿のコメントフィード';
+$GLOBALS['__l10n']['This post\'s comments Atom feed'] = 'この投稿のコメントのAtomフィード';
+$GLOBALS['__l10n']['Attachments'] = '添付ファイル';
+$GLOBALS['__l10n']['Permalink'] = 'パーマリンク';
+$GLOBALS['__l10n']['Comments'] = 'コメント';
+$GLOBALS['__l10n']['Your comment'] = 'あなたのコメント';
+$GLOBALS['__l10n']['Your comment has been published.'] = 'コメントが掲載されました。';
+$GLOBALS['__l10n']['Your comment has been submitted and will be reviewed for publication.'] = 'コメントは正常に送信され、掲載待ちとなりました。';
+$GLOBALS['__l10n']['Add a comment'] = 'コメントを追加';
+$GLOBALS['__l10n']['Name or nickname'] = 'お名前／ニックネーム';
+$GLOBALS['__l10n']['Email address'] = 'メールアドレス';
+$GLOBALS['__l10n']['Website'] = 'ウェブサイト';
+$GLOBALS['__l10n']['optional'] = '任意';
+$GLOBALS['__l10n']['Comment'] = 'コメント';
+$GLOBALS['__l10n']['HTML code is displayed as text and web addresses are automatically converted.'] = 'HTMLコードはテキストとして表示され、ウェブアドレスは自動的に変換されます。';
+$GLOBALS['__l10n']['Remember me on this blog'] = '次回から入力を省略';
+$GLOBALS['__l10n']['preview'] = 'プレビュー';
+$GLOBALS['__l10n']['send'] = '送信';
+$GLOBALS['__l10n']['They posted on the same topic'] = '同じトピックに関する投稿';
+$GLOBALS['__l10n']['Trackback URL'] = 'トラックバックURL';
+$GLOBALS['__l10n']['You must provide an author name'] = '投稿者名の入力は必須です';
+$GLOBALS['__l10n']['You must provide a comment'] = 'コメントの入力は必須です';
+$GLOBALS['__l10n']['Email address is not valid'] = 'メールアドレスが無効です';
+$GLOBALS['__l10n']['Document not found'] = 'ドキュメントが見つかりませんでした';
+$GLOBALS['__l10n']['The document you are looking for does not exist.'] = 'お探しのドキュメントは存在しません。';
+$GLOBALS['__l10n']['Powered by %s'] = 'Powered by %s';
+$GLOBALS['__l10n']['Subscribe to'] = '定期受信：';
+$GLOBALS['__l10n']['What is an RSS feed?'] = 'RSSフィードとは？';
+$GLOBALS['__l10n']['RSS feed is a free blog summary. It provides content (either posts or comments) or summaries of content, together with links to the full versions, and other metadata. The last published items may then be read by your favorite RSS <a href="http://en.wikipedia.org/wiki/Aggregator">aggregator</a>.'] = 'RSSフィードはフリーなブログの要約です。 それはフルバージョン（全文）や他のメタデータへのリンクと共に、コンテンツ（投稿やコメント）やコンテンツの要約を提供します。 これによって、最近のアイテムをあなたのお好みのRSS<a href="http://ja.wikipedia.org/wiki/Aggregator">アグリゲータ</a>で読むことができます。';
+$GLOBALS['__l10n']['Simply copy the following URL into your aggregator:'] = 'あなたのアグリゲータに以下のURLをコピーしてください：';
+$GLOBALS['__l10n']['Password needed'] = 'パスワードが必要です';
+$GLOBALS['__l10n']['You must give a password to access this area.'] = 'このエリアへのアクセスにはパスワードが必要です。';
+$GLOBALS['__l10n']['Password:'] = 'パスワード：';
+$GLOBALS['__l10n']['You must provide a valid email address.'] = '有効なメールアドレスを入力する必要があります。';
+$GLOBALS['__l10n']['Read'] = '読む';
+?>
